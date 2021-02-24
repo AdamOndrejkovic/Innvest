02d07c8f890b13e43c8f96c0df0e0b366e32939d
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-02-24 18:07:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+echo '
+<!DOCTYPE html>
+<html>
+<title>InnVest</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body id="homePage">
+
+<nav class="navbar navbar-expand-lg" id="navbarBox" style="margin-bottom: 30px">
+  <a class="navbar-brand" href="#"><img src="site/pictures/logo.png" alt="InnVest logo" width="150px" height="50px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class=""><svg xmlns="http://www.w3.org/2000/svg" width="36px" height="36px" fill="rgba(36,54,101,1)" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+</svg></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbar">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <a href="" class="my-2 my-lg-0" id="logBtn">
+      Login
+    </a>
+  </div>
+</nav>
+
+<div class="row m-0" id="introHeaderBox">
+<div class="col-sm-8" id="introHeader">
+<div class="w3-container w3-animate-zoom">
+  <h1>Welcome to Innvest</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aspernatur laudantium. </p>
+ 
+</div>
+
+</div>
+</div>
+
+<div class="row m-0" id="introTextBox">
+<div class="col-lg-6" id="introText">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam nesciunt optio vitae! Blanditiis deserunt, 
+dolore ducimus earum explicabo impedit itaque laborum natus nisi officia quaerat quas quod sapiente sit suscipit. <br>
+<a href="" class="btn-info">Learn More</a>
+</p>
+
+</div>
+</div>
+
+
+
+
+</body>
+</html>
+';
+
